18a93ad5a418597a57e297ab6f1095037649605e
Rade Zheng <zhzhen@microsoft.com>
2018-12-18 12:10:53 +0800

    firt commit

---
+++ github.cmd
@@ -0,0 +1,6 @@
+@REM git init
+
+git add *
+git commit -m "firt commit"
+git remote add origin https://github.com/radezheng/uuid-generator.git
+git push -u origin master
---
+++ run.cmd
@@ -0,0 +1,2 @@
+set FLASK_APP=app/__init__.py 
+flask run
