89ca68019ec0fc50db570cdb476ef43fcaa5bec6
Rade Zheng <zhzhen@microsoft.com>
2018-12-18 13:40:56 +0800

    add brigade.js

--- github.cmd
+++ github.cmd
@@ -1,6 +1,12 @@
 @REM git init
 
-git add *
-git commit -m "firt commit"
+echo "# uuid-generator" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/radezheng/uuid-generator.git
+git push -u origin master
+
+@REM or existing code
 git remote add origin https://github.com/radezheng/uuid-generator.git
 git push -u origin master
